21cc2d5d690634111abd7953c0b7cc2282cc1394
Deniz Umut Dereli <deniz.umut.dereli@gmail.com>
2022-01-07 17:20:22 +0300

    es6 and mongo-atlas connection settled

---
+++ helper/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const mongoOptions = {
+    autoIndex: true, //this is the code I added that solved it all
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    //useFindAndModify: false, 
+  }
+
+module.exports = () => {
+    mongoose.connect('mongodb+srv://movie_user:SHgK04KnbZkZJjoQ@mcluster.yuskm.mongodb.net/MCluster?retryWrites=true&w=majority', mongoOptions)
+
+    console.log('function trigger');
+
+    mongoose.connection.on('open', ()=> {
+
+        console.log('MongoDB:Connected');
+
+    })
+    mongoose.connection.on('error', (err)=> {
+
+        console.log('MongoDB: Not Connected ' + err);
+
+    })
+}
